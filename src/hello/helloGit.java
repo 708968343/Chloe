bef046d848265868e0a0f86a01bf1a422b52fa96
振洋 于 <708968343@qq.com>
2018-12-19 10:30:47 +0800

    第三次提交

--- src/hello/helloGit.java
+++ src/hello/helloGit.java
@@ -2,7 +2,10 @@ package hello;
 
 public class helloGit {
     public static void main(String[] args) {
-        /**/
+        /*1.0*/
         System.out.println("hello git");
+
+        /*2.0*/
+        System.out.println("hello git2");
     }
 }
